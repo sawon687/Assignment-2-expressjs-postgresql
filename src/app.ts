ed30d1220987f996da9b081c9042d1c8ddca906d
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-05-23 19:21:36 +0600

    added final update sum

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,7 @@ import { issuesRoute } from './modules/issues/issues.route'
 const app = express()
 app.use(express.json())
 app.use(logger)
-const corsOptions = {
-  origin: 'http://localhost:8000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+
 
 app.use('/api/auth',authRoute)
 app.use('/api/issues',issuesRoute)
